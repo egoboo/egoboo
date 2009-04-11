447e3b7183f4964fafcc5709385a400015d02962
zefz <zefz@8ff2ee9d-8245-0410-912a-a7eda06b689b>
2009-04-11 19:14:47 +0000

    - Forgot to add these new header files :)

---
+++ game/enchant.h
@@ -0,0 +1,93 @@
+#pragma once
+
+//********************************************************************************************
+//*
+//*    This file is part of Egoboo.
+//*
+//*    Egoboo is free software: you can redistribute it and/or modify it
+//*    under the terms of the GNU General Public License as published by
+//*    the Free Software Foundation, either version 3 of the License, or
+//*    (at your option) any later version.
+//*
+//*    Egoboo is distributed in the hope that it will be useful, but
+//*    WITHOUT ANY WARRANTY; without even the implied warranty of
+//*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//*    General Public License for more details.
+//*
+//*    You should have received a copy of the GNU General Public License
+//*    along with Egoboo.  If not, see <http:// www.gnu.org/licenses/>.
+//*
+//********************************************************************************************
+
+/* Egoboo - enchant.h
+ * Decleares some stuff used for handling enchants
+ */
+
+#include "egobootypedef.h"
+
+//Different set values for enchants
+typedef enum enchant_set
+{
+	SETDAMAGETYPE = 0,		//Type of damage dealt
+    SETNUMBEROFJUMPS,		//Max number of jumps
+	SETLIFEBARCOLOR,		//Color of life bar
+	SETMANABARCOLOR,		//Color of mana bar
+	SETSLASHMODIFIER,      // Damage modifiers
+    SETCRUSHMODIFIER,
+    SETPOKEMODIFIER,
+    SETHOLYMODIFIER,
+    SETEVILMODIFIER,
+    SETFIREMODIFIER,
+    SETICEMODIFIER,
+    SETZAPMODIFIER,
+    SETFLASHINGAND,				//Flash rate
+    SETLIGHTBLEND,				//Transparency
+    SETALPHABLEND,				//Alpha
+    SETSHEEN,				    // Shinyness
+    SETFLYTOHEIGHT,				//Fly to this height
+    SETWALKONWATER,				//Walk on water?
+    SETCANSEEINVISIBLE,			//Can it see invisible?
+    SETMISSILETREATMENT,		//How to treat missiles
+    SETCOSTFOREACHMISSILE,		//Cost for each missile treat
+    SETMORPH,					// Morph character?
+    SETCHANNEL,					// Can channel life as mana?
+	MAX_ENCHANT_SET,
+};
+
+typedef enum enchant_add
+{
+	ADDJUMPPOWER = 0,
+	ADDBUMPDAMPEN,
+	ADDBOUNCINESS,
+	ADDDAMAGE,           
+	ADDSIZE,                 
+	ADDACCEL,                
+	ADDRED,                        // Red shift
+	ADDGRN,                        // Green shift
+	ADDBLU,                        // Blue shift
+	ADDDEFENSE,                    // Defence adjustments
+	ADDMANA,                 
+	ADDLIFE,                 
+	ADDSTRENGTH,             
+	ADDWISDOM,               
+	ADDINTELLIGENCE,         
+	ADDDEXTERITY,
+};
+
+
+//Prototypes
+void free_all_enchants();
+void getadd( int min, int value, int max, int* valuetoadd );
+void fgetadd( float min, float value, float max, float* valuetoadd );
+Uint16 get_free_enchant();
+Uint16 enchant_value_filled( Uint16 enchantindex, Uint8 valueindex );
+void remove_enchant( Uint16 enchantindex );
+void set_enchant_value( Uint16 enchantindex, Uint8 valueindex,
+                        Uint16 enchanttype );
+void add_enchant_value( Uint16 enchantindex, Uint8 valueindex,
+                        Uint16 enchanttype );
+Uint16 spawn_enchant( Uint16 owner, Uint16 target,
+                      Uint16 spawner, Uint16 enchantindex, Uint16 modeloptional );
+void load_one_enchant_type( const char* szLoadName, Uint16 profile );
+void unset_enchant_value( Uint16 enchantindex, Uint8 valueindex );
+void remove_enchant_value( Uint16 enchantindex, Uint8 valueindex );
---
+++ game/passage.h
@@ -0,0 +1,39 @@
+#pragma once
+
+//********************************************************************************************
+//*
+//*    This file is part of Egoboo.
+//*
+//*    Egoboo is free software: you can redistribute it and/or modify it
+//*    under the terms of the GNU General Public License as published by
+//*    the Free Software Foundation, either version 3 of the License, or
+//*    (at your option) any later version.
+//*
+//*    Egoboo is distributed in the hope that it will be useful, but
+//*    WITHOUT ANY WARRANTY; without even the implied warranty of
+//*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//*    General Public License for more details.
+//*
+//*    You should have received a copy of the GNU General Public License
+//*    along with Egoboo.  If not, see <http:// www.gnu.org/licenses/>.
+//*
+//********************************************************************************************
+
+/* Egoboo - passage.h
+ * Passages and doors and whatnot.  Things that impede your progress!
+ */
+
+//Passage prototypes
+int open_passage( Uint16 passage );
+void check_passage_music();
+int break_passage( struct s_script_state * pstate, Uint16 passage, Uint16 starttile, Uint16 frames,
+                   Uint16 become, Uint8 meshfxor );
+void flash_passage( Uint16 passage, Uint8 color );
+Uint8 find_tile_in_passage( struct s_script_state * pstate, Uint16 passage, int tiletype );
+Uint16 who_is_blocking_passage( Uint16 passage );
+Uint16 who_is_blocking_passage_ID( Uint16 passage, IDSZ idsz );
+int close_passage( Uint16 passage );
+void clear_passages();
+void add_shop_passage( Uint16 owner, Uint16 passage );
+void add_passage( int tlx, int tly, int brx, int bry, Uint8 open, Uint8 mask );
+
