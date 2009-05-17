4502b12368d61a274126b16eb1b7bbe7ed0fb04d
bbirdsey <bbirdsey@8ff2ee9d-8245-0410-912a-a7eda06b689b>
2009-05-17 06:29:54 +0000

    - I made a function that will wrap a string so that it will be less than blah characters wide on a single line. Applying this to the end text allows it to display properly - Play tested the menu and beating the G'Nome module definitely does take you to the load players menu. The immersive thing would be to activate a link to Zippy City, as it says in the end text, but oh well...

--- game/graphic.c
+++ game/graphic.c
@@ -136,6 +136,8 @@ static void font_release();
 static void project_view(camera_t * pcam);
 static void make_prtlist( void );
 
+static void str_add_linebreaks( const char * text, size_t text_len, size_t line_len );
+
 //--------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------
 void EnableTexturing()
@@ -923,23 +925,68 @@ void reset_end_text()
     // ZZ> This function resets the end-module text
     if ( numpla > 1 )
     {
-        sprintf( endtext, "Sadly, they were never heard from again..." );
-        endtextwrite = 42;  // Where to append further text
+        endtextwrite = sprintf( endtext, "Sadly, they were never heard from again..." );
     }
     else
     {
         if ( numpla == 0 )
         {
             // No players???
-            sprintf( endtext, "The game has ended..." );
-            endtextwrite = 21;
+            endtextwrite = sprintf( endtext, "The game has ended..." );
         }
         else
         {
             // One player
-            sprintf( endtext, "Sadly, no trace was ever found..." );
-            endtextwrite = 33;  // Where to append further text
+            endtextwrite = sprintf( endtext, "Sadly, no trace was ever found..." );
         }
+    }
+
+    str_add_linebreaks( endtext, endtextwrite, 20 );
+}
+
+//--------------------------------------------------------------------------------------------
+void str_add_linebreaks( const char * text, size_t text_len, size_t line_len )
+{
+    char * text_end, * text_break, * text_stt;
+
+    if( NULL == text || '\0' == text[0] || 0 == text_len  || 0 == line_len ) return;
+
+    text_end = text + text_len;
+    text_break = text_stt = text;
+    while(text < text_end && '\0' != *text)
+    {
+        // scan for the next whitespace
+        text = strpbrk(text, " \n");
+        if( '\0' == text ) 
+        {
+            // reached the end of the string
+            break;
+        }
+        else if ( '\n' == *text )
+        {
+            // respect existing line breaks
+            text_break = text;
+            text++;
+            continue;
+        }
+
+        // until the line is too long, then insert
+        // replace the last good ' ' with '\n'
+        if( ((size_t)text - (size_t)text_break) > line_len )
+        {
+            if( ' ' != text_break )
+            {
+                text_break = text;   
+            }
+
+            // convert the character
+            *text_break = '\n';
+
+            // start over again
+            text = text_break;
+        }
+
+        text++;
     }
 }
 
@@ -1151,6 +1198,8 @@ void append_end_text( script_state_t * pstate, int message, Uint16 character )
     }
 
     endtext[endtextwrite] = 0;
+
+    str_add_linebreaks( endtext, endtextwrite, 20 );
 }
 
 //--------------------------------------------------------------------------------------------
