642b527076de7162d30b9b5741004143f7b5e9d6
penguinflyer5234 <penguinflyer5234@gmail.com>
2014-12-16 11:47:34 -0600

    C++ mode go! Also update mac file code to new Egoboo standards.

--- egolib/src/egolib/platform/file_mac.m
+++ egolib/src/egolib/platform/file_mac.m
@@ -2,6 +2,8 @@
 
 // Egoboo, Copyright (C) 2000 Aaron Bishop
 
+#include "egolib/file_common.h"
+
 #import <Foundation/NSPathUtilities.h>
 #import <Foundation/NSFileManager.h>
 #import <Foundation/NSBundle.h>
@@ -10,12 +12,12 @@
 #include <SDL.h>
 #include <SDL_endian.h>
 
-// Proto.h is not included in this file because Egoboo's BOOL conflicts
-// with Objective-C's built-in BOOL
-
-NSDirectoryEnumerator* fs_dirEnum = nil;
-NSString *fs_dirEnumExtension = nil;
-NSString *fs_dirEnumPath = nil;
+struct s_mac_find_context {
+    NSDirectoryEnumerator * dirEnum;
+    NSString * dirEnumExtension;
+    NSString * dirEnumPath;
+    NSString * currentFile;
+};
 
 #define MAX_PATH 260
 static NSString *binaryPath = nil;
@@ -26,7 +28,7 @@ static NSString *userPath = nil;
 //---------------------------------------------------------------------------------------------
 //File Routines-------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------
-void sys_fs_init()
+void sys_fs_init(const char * root_path)
 {
 	// JF> This function determines the temporary, import,
 	// game data and save paths
@@ -35,11 +37,16 @@ void sys_fs_init()
 	dataPath = [binaryPath stringByAppendingString:@"/Contents/Resources"];
 	userPath = [[NSFileManager defaultManager] documentsDirectory];
 //	configPath = [[NSFileManager defaultManager] applicationSupportDirectory];
+    
+    [binaryPath retain];
+    [dataPath retain];
+    [userPath retain];
 
 	NSLog(@"sys_fs_init: Game directory is %@", binaryPath);
 	NSLog(@"sys_fs_init: Data directory is %@", dataPath);
 	NSLog(@"sys_fs_init: User directory is %@", userPath);
 	NSLog(@"sys_fs_init: Config directory is %@", dataPath);
+    NSLog(@"sys_fs_init: got a thing: %s", root_path);
 }
 
 const char *fs_getBinaryDirectory()
@@ -95,34 +102,36 @@ void fs_deleteFile(const char *fileName)
 	[path release];
 }
 
-void fs_copyFile(const char *source, const char *dest)
+C_BOOLEAN fs_copyFile(const char *source, const char *dest)
 {
+    BOOL didCopy;
 	NSString *srcPath, *destPath;
 
 	srcPath = [[NSString alloc] initWithUTF8String:source];
 	destPath = [[NSString alloc] initWithUTF8String:dest];
 
-	NSLog(@"Copying file\n%@\nto\n%@\n", srcPath, destPath);
-	if ([[NSFileManager defaultManager] copyItemAtPath:srcPath toPath:destPath error:nil] != YES)
-	{
-		NSLog(@"Failed to copy!");
-	}
+    didCopy = [[NSFileManager defaultManager] copyItemAtPath:srcPath toPath:destPath error:nil];
 
 	[srcPath release];
 	[destPath release];
+    return didCopy == YES;
 }
 
 int fs_fileIsDirectory(const char *filename)
 {
 	// Returns 1 if this file is a directory
+    BOOL fileExists;
 	BOOL isDir = NO;
 	NSString *path;
 	NSFileManager *manager;
 
 	path = [[NSString alloc] initWithUTF8String: filename];
 	manager = [NSFileManager defaultManager];
+    
+    fileExists = [manager fileExistsAtPath:path isDirectory:&isDir];
+    [path release];
 
-	if ([manager fileExistsAtPath: path isDirectory: &isDir] && isDir)
+	if (fileExists && isDir)
 	{
 		return 1;
 	}
@@ -136,33 +145,42 @@ int fs_fileIsDirectory(const char *filename)
 //---------------------------------------------------------------------------------------------
 
 // Return the next file in the directory that matches the criteria specified in fs_findFirstFile
-const char *fs_findNextFile()
+const char *fs_findNextFile( fs_find_context_t * fs_search )
 {
 	NSString *fileName;
 	NSString *pathName;
+    
+    if (fs_search == NULL || fs_search->ptr.m == NULL || fs_search->type != mac_find)
+        return NULL;
+    
+    struct s_mac_find_context *context = fs_search->ptr.m;
 
-	if(fs_dirEnum == nil)
-	{
-		return NULL;
-	}
-
-	while (( fileName = [fs_dirEnum nextObject] ))
+	while (( fileName = [context->dirEnum nextObject] ))
 	{
 		// Also, don't go down directories recursively.
-		pathName = [NSString stringWithFormat: @"%@/%@", fs_dirEnumPath, fileName];
+		pathName = [NSString stringWithFormat: @"%@/%@", context->dirEnumPath, fileName];
 		if(fs_fileIsDirectory([pathName UTF8String]))
 		{
-			[fs_dirEnum skipDescendents];
+			[context->dirEnum skipDescendents];
 		}
 
-		if(fs_dirEnumExtension != nil)
+		if(context->dirEnumExtension != nil)
 		{
-			if ([[fileName pathExtension] isEqualToString: fs_dirEnumExtension])
+			if ([[fileName pathExtension] isEqualToString: context->dirEnumExtension])
 			{
+                if (context->currentFile != nil)
+                    [context->currentFile release];
+                context->currentFile = fileName;
+                [context->currentFile retain];
 				return [fileName UTF8String];
 			}
-		} else
-		{
+		}
+        else
+        {
+            if (context->currentFile != nil)
+                [context->currentFile release];
+            context->currentFile = fileName;
+            [context->currentFile retain];
 			return [fileName UTF8String];
 		}
 	}
@@ -171,65 +189,72 @@ const char *fs_findNextFile()
 }
 
 // Stop the current find operation
-void fs_findClose()
+void fs_findClose( fs_find_context_t * fs_search )
 {
-	if (fs_dirEnum != nil)
-	{
-		[fs_dirEnum release];
-		fs_dirEnum = nil;
-	}
+    if (fs_search == NULL || fs_search->ptr.m == NULL || fs_search->type != mac_find)
+        return;
+    
+    struct s_mac_find_context *context = fs_search->ptr.m;
+    
+	if (context->dirEnum != nil)
+		[context->dirEnum release];
 
-	if (fs_dirEnumPath != nil)
-	{
-		[fs_dirEnumPath release];
-		fs_dirEnumPath = nil;
-	}
+	if (context->dirEnumPath != nil)
+        [context->dirEnumPath release];
 
-	if (fs_dirEnumExtension != nil)
-	{
-		[fs_dirEnumExtension release];
-		fs_dirEnumExtension = nil;
-	}
+	if (context->dirEnumExtension != nil)
+		[context->dirEnumExtension release];
+    
+    if (context->currentFile != nil)
+        [context->currentFile release];
+    
+    EGOBOO_DELETE(context);
 }
 
 // Begin enumerating files in a directory.  The enumeration is not recursive; subdirectories
 // won't be searched.  If 'extension' is not NULL, only files with the given extension will
 // be returned.
-const char *fs_findFirstFile(const char *path, const char *extension)
+const char *fs_findFirstFile( const char *path, const char *extension, fs_find_context_t * fs_search )
 {
 	NSString *searchPath;
+    
+    if (fs_search == NULL)
+        return NULL;
 
-	if(fs_dirEnum != nil)
-	{
-		fs_findClose();
-	}
-
+    fs_search->type = mac_find;
+    fs_search->ptr.m = EGOBOO_NEW(struct s_mac_find_context);
+    if (fs_search->ptr.m == NULL)
+        return NULL;
+    
+    struct s_mac_find_context *context = fs_search->ptr.m;
+    
 	// If the path given is a relative one, we need to derive the full path
 	// for it by appending the current working directory
 	if(path[0] != '/')
 	{
-		searchPath = [NSString stringWithFormat: @"%@/%s", dataPath, path];
+		searchPath = [[NSString alloc] initWithFormat:@"%@/%s", dataPath, path];
 	} else
 	{
-		searchPath = [NSString stringWithUTF8String: path];
+		searchPath = [[NSString alloc] initWithUTF8String:path];
 	}
 
-	fs_dirEnum = [[NSFileManager defaultManager] enumeratorAtPath: searchPath];
-
-
-	if(extension != NULL)
+	context->dirEnum = [[NSFileManager defaultManager] enumeratorAtPath:searchPath];
+	if(context->dirEnum == nil)
 	{
-		fs_dirEnumExtension = [NSString stringWithUTF8String: extension];
-	}
-
-	fs_dirEnumPath = searchPath;
-
-	if(fs_dirEnum == nil)
-	{
+        [searchPath release];
+        EGOBOO_DELETE(context);
+        fs_search->type = unknown_find;
 		return NULL;
 	}
 
-	return fs_findNextFile();
+	if(extension != NULL)
+	{
+		context->dirEnumExtension = [[NSString alloc] initWithUTF8String:extension];
+	}
+
+	context->dirEnumPath = searchPath;
+
+	return fs_findNextFile( fs_search );
 }
 
 void empty_import_directory()
