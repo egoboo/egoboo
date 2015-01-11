7874ca4c154b6140f5443dc6da26bc3770fed9f8
Zefz <zefzsoftwares@hotmail.com>
2015-01-11 02:35:04 +0100

    Passage: Added missing files

---
+++ game/src/game/module/Passage.hpp
@@ -0,0 +1,158 @@
+//********************************************************************************************
+//*
+//*    This file is part of Egoboo.
+//*
+//*    Egoboo is free software: you can redistribute it and/or modify it
+//*    under the terms of the GNU General Public License as published by
+//*    the Free Software Foundation, either version 3 of the License, or
+//*    (at your option) any later version.
+//*
+//*    Egoboo is distributed in the hope that it will be useful, but
+//*    WITHOUT ANY WARRANTY; without even the implied warranty of
+//*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//*    General Public License for more details.
+//*
+//*    You should have received a copy of the GNU General Public License
+//*    along with Egoboo.  If not, see <http://www.gnu.org/licenses/>.
+//*
+//********************************************************************************************
+
+/// @file game/Passage.hpp
+/// @details Passages and doors and whatnot.  Things that impede your progress!
+/// @author Johan Jansen
+
+#pragma once
+
+#include "egolib/typedef.h"
+#include "game/egoboo_typedef.h"
+
+//Forward declarations
+struct chr_t;
+
+class Passage
+{
+public:
+	static constexpr int32_t NO_MUSIC = -1;				///< For passages that play no music
+	static constexpr uint32_t CLOSE_TOLERANCE = 3;		///< For closing doors
+	static constexpr size_t MAX_PASSAGES = 256;			///< Maximum allowed passages
+	static constexpr CHR_REF SHOP_NOOWNER = 0xFFFF;		///< Shop has no owner
+	static constexpr uint32_t SHOP_STOLEN = 0xFFFF;
+
+	/// The pre-defined orders for communicating with shopkeepers
+	enum ShopOrders : uint8_t
+	{
+	    SHOP_BUY,
+	    SHOP_SELL,
+	    SHOP_NOAFFORD,
+	    SHOP_THEFT,
+	    SHOP_ENTER,
+	    SHOP_EXIT,
+
+	    SHOP_LAST
+	};
+
+	/**
+	* @brief Default constructor
+	**/
+	Passage();
+
+	/**
+	* @brief Constructor
+	**/
+	Passage(const irect_t& area, const uint8_t mask);
+
+	/**
+	* @brief get top left coordinate of passage
+	**/
+	inline int getLeft() const {return _area.left;}
+
+	/**
+	* @brief get top right coordinate of passage
+	**/
+	inline int getTop() const {return _area.top;}
+
+	/**
+	* @brief get bottom right coordinate of passage
+	**/
+	inline int getRight() const {return _area.right;}
+
+	/**
+	* @brief get bottom left coordinate of passage
+	**/
+	inline int getBottom() const {return _area.bottom;}
+
+	/**
+	* @brief returns true if this passage is currently open (not impassable)
+	**/
+	bool isOpen() const;
+
+	/**
+	* @brief opens this passage
+	**/
+	void open();
+
+	/**
+	* @brief closes this passage
+	* @return false if it failed to close (incrushable character inside for example)
+	**/
+	bool close();
+
+    /**
+    * @brief This return true if the specified X and Y coordinates are within the passage
+    *    	 radius is how much offset we allow outside the passage
+    * @return true if object is inside this passage
+    */
+
+	bool objectIsInPassage(float xpos, float ypos, float radius) const;
+
+	/**
+	* @brief This function makes a passage flash the specified color
+	**/
+	void flashColor(uint8_t color);
+
+    /**
+    * @brief This function returns INVALID_CHR_REF if there is no character in the passage,
+    *    	 otherwise the index of the first character found is returned...
+    *    	 Can also look for characters with a specific quest or item in his or her inventory
+    *    	 Finds living ones, then items and corpses
+    * @return CHR_REF of the character found which fullfills all specified requirements or INVALID_CHR_REF if none found
+    **/
+    CHR_REF whoIsBlockingPassage( const CHR_REF isrc, IDSZ idsz, const BIT_FIELD targeting_bits, IDSZ require_item ) const;
+
+
+    /**
+    * @brief This return true if the specified X and Y coordinates are within the passage
+    **/
+    bool isPointInside(float x, float y) const;
+
+    /**
+    * @brief Plays the passage music assigned to this passage if the specified character is inside this passage
+    * @return true if a new song is now playing
+    **/
+    bool checkPassageMusic(const chr_t * pchr) const;
+
+    /**
+    * @return Sets the MusicID of this passage. If a player character enters this passage, the specified MusicID will be played
+    **/
+    void setMusic(const int32_t musicID);
+
+    /**
+    * @return true if this passage is a shop
+    **/
+    bool isShop() const;
+
+    CHR_REF getShopOwner() const;
+
+    void makeShop(CHR_REF owner);
+
+    void removeShop();
+
+private:
+    irect_t _area;			///< Passage area
+    int32_t _music;   		///< Music track appointed to the specific passage
+    uint8_t _mask;  		///< Is it IMPASSABLE, SLIPPERY or whatever
+    bool _open;   			///< Is the passage open?
+
+    bool _isShop;			///< True if this passage is a shop
+    CHR_REF _shopOwner;		///< CHR_REF of the owner of this shop
+};
---
+++ game/src/game/module/PassageHandler.cpp
@@ -0,0 +1,143 @@
+#include <vector>
+#include "game/module/PassageHandler.hpp"
+
+#include "egolib/vfs.h"
+#include "egolib/fileutil.h"
+
+#include "game/game.h"
+#include "game/player.h"
+#include "game/char.inl"
+
+namespace Passages
+{
+	//Private global
+	static std::vector<std::shared_ptr<Passage>> _passages;
+
+	void clearPassages()
+	{
+		_passages.clear();
+	}
+
+	void loadAllPassages()
+	{
+	    // Reset all of the old passages
+	    _passages.clear();
+
+	    // Load the file
+	    vfs_FILE *fileread = vfs_openRead( "mp_data/passage.txt" );
+	    if ( NULL == fileread ) return;
+
+	    //Load all passages in file
+	    while ( goto_colon_vfs( NULL, fileread, true ) )
+	    {
+	    	//read passage area
+	    	irect_t area;
+	        area.left   = vfs_get_int( fileread );
+	        area.top    = vfs_get_int( fileread );
+	        area.right  = vfs_get_int( fileread );
+	        area.bottom = vfs_get_int( fileread );
+
+	        //constrain passage area within the level
+	        area.left    = CLIP( area.left,   0, PMesh->info.tiles_x - 1 );
+    		area.top     = CLIP( area.top,    0, PMesh->info.tiles_y - 1 );
+    		area.right   = CLIP( area.right,  0, PMesh->info.tiles_x - 1 );
+    		area.bottom  = CLIP( area.bottom, 0, PMesh->info.tiles_y - 1 );
+
+	        //Read if open by default
+	        bool open = vfs_get_bool( fileread );
+
+	        //Read mask (optional)
+	        uint8_t mask = MAPFX_IMPASS | MAPFX_WALL;
+	        if ( vfs_get_bool( fileread ) ) mask = MAPFX_IMPASS;
+	        if ( vfs_get_bool( fileread ) ) mask = MAPFX_SLIPPY;
+
+	        std::shared_ptr<Passage> passage = std::make_shared<Passage>(area, mask);
+
+	        //check if we need to close the passage
+	        if(!open) {
+	        	passage->close();
+	        }
+
+	        //finished loading this one!
+	   		_passages.push_back(passage);
+	    }
+
+	    //all done!
+	    vfs_close( fileread );
+	}
+
+	void checkPassageMusic()
+	{
+        // Look at each player
+        for ( PLA_REF ipla = 0; ipla < MAX_PLAYER; ipla++ )
+        {
+            CHR_REF character = PlaStack.lst[ipla].index;
+            if ( !INGAME_CHR( character ) ) continue;
+
+            //dont do items in hands or inventory
+            if ( IS_ATTACHED_CHR( character ) ) continue;
+
+            chr_t * pchr = ChrList_get_ptr( character );
+            if ( !pchr->alive || !VALID_PLA( pchr->is_which_player ) ) continue;
+
+		    //Loop through every passage
+		    for(const std::shared_ptr<Passage> &passage : _passages)
+		    {
+	    		if(passage->checkPassageMusic(pchr)) {
+	    			return;
+	    		}
+		    }	
+        }
+	}
+
+	CHR_REF getShopOwner(const float x, const float y)
+	{
+		//Loop through every passage
+		for(const std::shared_ptr<Passage> &passage : _passages)
+	    {
+	    	//Only check actual shops
+	    	if(!passage->isShop()) {
+	    		continue;
+	    	}
+
+	    	//Is item inside this shop?
+	    	if(passage->isPointInside(x, y)) {
+	    		return passage->getShopOwner();
+	    	}
+	    }
+
+	    return Passage::SHOP_NOOWNER;		
+	}
+
+	void removeShopOwner(CHR_REF owner)
+	{
+		//Loop through every passage
+		for(const std::shared_ptr<Passage> &passage : _passages)
+	    {
+	    	//Only check actual shops
+	    	if(!passage->isShop()) {
+	    		continue;
+	    	}
+
+	    	if(passage->getShopOwner() == owner) {
+	    		passage->removeShop();
+	    	}
+
+	    	//TODO: mark all items in shop as normal items again
+	    }
+	}
+
+	int getPassageCount()
+	{
+		return _passages.size();
+	}
+
+	std::shared_ptr<Passage> getPassageByID(int id)
+	{
+		if(id < 0 || id >= _passages.size()) {
+			return nullptr;
+		}
+
+		return _passages[id];
+	}
+}
