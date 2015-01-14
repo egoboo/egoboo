bb3aefe9ad5aa4a175e9fd74559e4369f3e43246
penguinflyer5234 <penguinflyer5234@gmail.com>
2015-01-14 15:32:29 -0600

    Actually fix ambient lighting this time plus other assorted lighting bugs. Fix water flickering. Fix phong rendering.

--- game/src/game/graphic.c
+++ game/src/game/graphic.c
@@ -5818,26 +5818,28 @@ float get_ambient_level()
     if ( gfx.usefaredge )
     {
         // for outside modules, max light_a means bright sunlight
-        glob_amb = light_a;
+        // this should be handled with directional lighting, so ambient light is 0
+        //glob_amb = light_a * 255.0f;
+        glob_amb = 0;
     }
     else
     {
         // for inside modules, max light_a means dingy dungeon lighting
-        glob_amb = light_a / 8.0f;
+        glob_amb = light_a * 32.0f;
     }
 
     // determine the minimum ambient, based on darkvision
-    min_amb = INVISIBLE / 4 / 256.0f;
+    min_amb = INVISIBLE / 4;
     if ( local_stats.seedark_mag != 1.0f )
     {
         // start with the global light
         min_amb  = glob_amb;
 
         // give a iny boost in the case of no light_a
-        if ( local_stats.seedark_mag > 0.0f ) min_amb += 1.0f / 256.0f;
+        if ( local_stats.seedark_mag > 0.0f ) min_amb += 1.0f;
 
         // light_a can be quite dark, so we need a large magnification
-        min_amb *= std::pow(1 + local_stats.seedark_mag, 5);
+        min_amb *= std::pow(local_stats.seedark_mag, 5);
     }
 
     return std::max( glob_amb, min_amb );
