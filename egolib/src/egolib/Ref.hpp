cd9e7899b04f922523808ffed0bf4e6cab2d0d91
Michael Heilmann <michaelheilmann@primordialmachine.com>
2015-02-12 12:12:02 +0100

    Add Ref type

---
+++ egolib/src/egolib/Ref.hpp
@@ -0,0 +1,119 @@
+#pragma once
+
+/// @todo Assert that MIN <= INVALID <= MAX upon template instantiation. 
+template <size_t MIN,size_t MAX,size_t INVALID>
+struct Ref
+{
+private:
+    size_t _ref;
+public:
+
+	Ref() :
+		_ref(INVALID)
+	{
+	}
+
+    Ref(const Ref<MIN, MAX, INVALID>& other) :
+        _ref(other._ref)
+    {
+    }
+		
+	bool operator!() const
+	{
+		return _ref == INVALID;
+	}
+
+	// Relational operator overloads.
+	
+    bool operator>=(const Ref<MIN, MAX, INVALID>& other) const
+    {
+        return _ref >= other._ref;
+    }
+
+    bool operator<=(const Ref<MIN, MAX, INVALID>& other) const
+    {
+        return _ref <= other._ref;
+    }
+
+    bool operator<(const Ref<MIN, MAX, INVALID>& other) const
+    {
+        return _ref < other._ref;
+    }
+
+    bool operator>(const Ref<MIN, MAX, INVALID>& other) const
+    {
+        return _ref > other._ref;
+    }
+
+    bool operator==(const Ref<MIN, MAX, INVALID>& other) const
+    {
+        return _ref == other._ref;
+    }
+
+    bool operator!=(const Ref<MIN, MAX, INVALID>& other) const
+    {
+        return _ref != other._ref;
+    }
+	
+	// Assignment operator overloads.
+
+    Ref<MIN, MAX, INVALID>& operator=(const Ref<MIN, MAX, INVALID>& other)
+    {
+        _ref(other._ref);
+        return *this;
+    }
+
+    Ref<MIN, MAX, INVALID>& operator=(const unsigned int& other)
+    {
+        if (other < MIN || other > MAX)
+        {
+            throw std::domain_error();
+        }
+        _ref = other;
+        return *this;
+    }
+
+    Ref<MIN, MAX, INVALID>& operator=(const int& other)
+    {
+        if (other < MIN || other > MAX)
+        {
+            throw std::domain_error();
+        }
+        _ref = other;
+        return *this;
+    }
+
+	Ref<MIN, MAX, INVALID>& operator=(const size_t& other)
+	{
+		if (other < MIN || other > MAX)
+		{
+			throw std::domain_error();
+		}
+		_ref = other;
+        return *this;
+	}
+	
+	// Cast operator overloads.
+	
+    operator unsigned int() const
+    {
+        if (_ref > std::numeric_limit<unsigned int>::max())
+        {
+            throw std::bad_cast();
+        }
+    }
+
+    operator int() const
+    {
+        if (_ref > std::numeric_limit<int>::max())
+        {
+            throw std::bad_cast();
+        }
+    }
+
+	operator size_t() const
+	{
+		return _ref;
+	}
+	
+};
