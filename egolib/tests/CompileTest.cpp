19b9fb97df9c3ca5d8c89756eb9ead921eea746d
penguinflyer5234 <penguinflyer5234@gmail.com>
2015-03-10 04:12:46 -0500

    Add a dummy test for egolib.

---
+++ egolib/tests/CompileTest.cpp
@@ -0,0 +1,34 @@
+//********************************************************************************************
+//*
+//*    This file is part of Egoboo.
+//*
+//*    Egoboo is free software: you can redistribute it and/or modify it
+//*    under the terms of the GNU General Public License as published by
+//*    the Free Software Foundation, either version 3 of the License, or
+//*    (at your option) any later version.
+//*
+//*    Egoboo is distributed in the hope that it will be useful, but
+//*    WITHOUT ANY WARRANTY; without even the implied warranty of
+//*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//*    General Public License for more details.
+//*
+//*    You should have received a copy of the GNU General Public License
+//*    along with Egoboo.  If not, see <http://www.gnu.org/licenses/>.
+//*
+//********************************************************************************************
+
+#include "EgoTest/EgoTest.hpp"
+
+#include <iostream>
+
+EgoTest_DeclareTestCase(CompileTest)
+EgoTest_EndDeclaration()
+
+EgoTest_TestCase(CompileTest)
+
+EgoTest_Test(compileTest)
+{
+    EgoTest_Assert(std::cout << "Hello, World!\n");
+}
+
+EgoTest_EndTestCase()
