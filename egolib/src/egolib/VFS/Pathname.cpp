b8dc2a4b5f4c99bdac0b7e3b77eae12f309edcea
Michael Heilmann <michaelheilmann@primordialmachine.com>
2015-05-21 06:14:39 +0200

    Add missing files

---
+++ egolib/src/egolib/VFS/Pathname.cpp
@@ -0,0 +1,61 @@
+#include "egolib/VFS/Pathname.hpp"
+
+#include <regex>
+
+namespace Ego {
+namespace VFS {
+
+Pathname::Pathname(const std::string& string)
+    : _schemaName("ego"), _string(string) {
+    if (string.empty()) {
+        throw std::invalid_argument("invalid pathname string");
+    }
+    // pathnames may not contain `..` or `:`.
+    if (_string.find("..") != std::string::npos && _string.find(":") != std::string::npos) {
+        throw std::invalid_argument("invalid pathname string");
+    }
+    // pathnames may not contain `\`.
+    if (_string.find("\\") != std::string::npos) {
+        throw std::invalid_argument("invalid pathname string");
+    }
+    std::regex e("/+");
+    // Double slashes are eliminated.
+    _string = std::regex_replace(_string, e, "/");
+}
+
+Pathname::Pathname(const Pathname& other)
+    : _string(other._string) {
+}
+
+Pathname& Pathname::operator=(const Pathname& other) {
+    _string = other._string;
+    return *this;
+}
+
+bool Pathname::operator==(const Pathname& other) const {
+    return _schemaName == other._schemaName
+        && _string == other._string;
+}
+
+const std::string& Pathname::toString() const {
+    return _string;
+}
+
+std::string Pathname::getExtension() const {
+    auto pos = _string.rfind('.');
+    // No period found/period at the beginning (end) of the pathname string  ...
+    if (pos == std::string::npos || pos == 0 || pos == _string.length()) {
+        // ... no extension.
+        return std::string();
+    }
+    // Case exhaustion: the extension is non-empty.
+    return _string.substr(pos+1, _string.length() - pos - 1);
+}
+
+Pathname Pathname::operator+(const Pathname& other) const {
+    // Double-slashes are eliminated in the constructor.
+    return Pathname(_string + "/" + other._string);
+}
+
+} // namespace VFS
+} // namespace Ego
---
+++ egolib/src/egolib/VFS/Pathname.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+#include "egolib/platform.h"
+
+namespace Ego {
+namespace VFS {
+
+/**
+ * @brief
+ *  Represents a pathname in the virtual file system.
+ */
+struct Pathname {
+
+private:
+
+    /**
+     * @brief
+     *  The schema name.
+     *  Currently fixed to "ego".
+     */
+    std::string _schemaName;
+
+    std::string _string;
+
+public:
+
+    /**
+     * @brief
+     *  Construct this pathname with a string.
+     * @param string
+     *  the pathname string
+     */
+    Pathname(const std::string& string);
+
+    /**
+     * @brief
+     *  Construct this pathname with the values of another pathname.
+     * @param other
+     *  the other pathname
+     */
+    Pathname(const Pathname& other);
+
+    /**
+     * @brief
+     *  Assign this pathname with the values of another pathname.
+     * @param other
+     *  the other pathname
+     * @return
+     *  this pathname
+     */
+    Pathname& operator=(const Pathname& other);
+
+    /**
+     * @brief
+     *  Get if this pathname is equal to another pathname.
+     * @param other
+     *  the other pathname
+     * @return
+     *  @a true if this pathname is equal to the other pathname,
+     *  @false otherwise
+     */
+    bool operator==(const Pathname& other) const;
+    
+    /**
+     * @brief
+     *  Get the pathname as a string.
+     * @return
+     *  the string
+     */
+    const std::string& toString() const;
+
+    /**
+     * @brief
+     *  Get the extension of this pathname.
+     * @return
+     *  the extension of this pathname if the pathname has one,
+     *  the empty string otherwise
+     * @remark
+     *  The extension of a pathname is the part of the pathname starting after the last period character in the pathname.
+     *  However, if
+     *  - no period is found in the pathname or
+     *  - the period is the first character or the last character of the pathname,
+     *  then the extension is empty.
+     */
+    std::string getExtension() const;
+
+    /**
+     * @brief
+     *  Concatenate this pathname with another pathname.
+     * @param other
+     *  the other pathname
+     */
+    Pathname operator+(const Pathname& other) const;
+
+};
+
+} // namespace VFS
+} // namespace Ego
