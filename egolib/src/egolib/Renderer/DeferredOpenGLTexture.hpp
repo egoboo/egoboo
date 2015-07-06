8b535ec806fe23939cf4284924e2b19b253ea51a
Johan Jansen <jnsn.johan@gmail.com>
2015-07-07 00:49:30 +0200

    Renderer: Add deferred texture type

---
+++ egolib/src/egolib/Renderer/DeferredOpenGLTexture.hpp
@@ -0,0 +1,26 @@
+#include "egolib/Renderer/Texture.hpp"
+
+namespace Ego
+{
+    /**
+    * @brief
+    *   A OpenGL texture with lazy loading. This means the texture will not
+    *   be loaded into memory before it is required for rendering
+    **/
+    class DeferredOpenGLTexture
+    {
+    public:
+        DeferredOpenGLTexture(const std::string &filePath);
+
+        const oglx_texture_t& get();
+
+        void release();
+
+        void setTextureSource(const std::string &filePath);
+
+    private:
+        std::shared_ptr<oglx_texture_t> _texture;
+        std::string _filePath;
+        bool _loaded;
+    };
+}
