3ee9c09ccdb185eeac8a0ddb147eae5a8bb7fd6b
penguinflyer5234 <penguinflyer5234@gmail.com>
2015-08-07 02:06:43 -0500

    Add debugging object loading and font layout

---
+++ game/src/game/GameStates/DebugMainMenuState.cpp
@@ -0,0 +1,63 @@
+//********************************************************************************************
+//*
+//*    This file is part of Egoboo.
+//*
+//*    Egoboo is free software: you can redistribute it and/or modify it
+//*    under the terms of the GNU General Public License as published by
+//*    the Free Software Foundation, either version 3 of the License, or
+//*    (at your option) any later version.
+//*
+//*    Egoboo is distributed in the hope that it will be useful, but
+//*    WITHOUT ANY WARRANTY; without even the implied warranty of
+//*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//*    General Public License for more details.
+//*
+//*    You should have received a copy of the GNU General Public License
+//*    along with Egoboo.  If not, see <http://www.gnu.org/licenses/>.
+//*
+//********************************************************************************************
+
+/// @file game/GameStates/DebugMainMenuState.cpp
+/// @details State that contains buttons to all non-module related debugging states.
+/// @author Johan Jansen, penguinflyer5234
+
+#include "game/GameStates/DebugMainMenuState.hpp"
+#include "game/Core/GameEngine.hpp"
+#include "game/GUI/Button.hpp"
+
+#include "game/GameStates/DebugFontRenderingState.hpp"
+#include "game/GameStates/DebugModuleLoadingState.hpp"
+#include "game/GameStates/DebugObjectLoadingState.hpp"
+
+DebugMainMenuState::DebugMainMenuState() {
+    const int SCREEN_HEIGHT = _gameEngine->getUIManager()->getScreenHeight();
+    
+    auto button = std::make_shared<Button>("Back");
+    button->setPosition(5, SCREEN_HEIGHT - button->getHeight() - 5);
+    button->setWidth(150);
+    button->setOnClickFunction([this] { endState(); });
+    addComponent(button);
+    
+    int y = 5;
+    
+    button = std::make_shared<Button>("Load Modules");
+    button->setPosition(5, y);
+    button->setWidth(300);
+    button->setOnClickFunction([] { _gameEngine->pushGameState(std::make_shared<DebugModuleLoadingState>()); });
+    addComponent(button);
+    y += button->getHeight() + 10;
+    
+    button = std::make_shared<Button>("Load Objects");
+    button->setPosition(5, y);
+    button->setWidth(300);
+    button->setOnClickFunction([] { _gameEngine->pushGameState(std::make_shared<DebugObjectLoadingState>()); });
+    addComponent(button);
+    y += button->getHeight() + 10;
+    
+    button = std::make_shared<Button>("Font Rendering");
+    button->setPosition(5, y);
+    button->setWidth(300);
+    button->setOnClickFunction([] { _gameEngine->pushGameState(std::make_shared<DebugFontRenderingState>()); });
+    addComponent(button);
+    y += button->getHeight() + 10;
+}
