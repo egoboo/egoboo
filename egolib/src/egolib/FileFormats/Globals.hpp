6cbbf2c6faddc9b2670ef044aab4241d395fc1de
Michael Heilmann <michaelheilmann@primordialmachine.com>
2015-10-10 22:55:57 +0200

    Fix include directive for case-sensitive file systems

--- egolib/src/egolib/FileFormats/Globals.hpp
+++ egolib/src/egolib/FileFormats/Globals.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "egolib/FileFormats/map_tile_dictionary.h"
-#include "egolib/Fileformats/treasure_table_file.h"
+#include "egolib/FileFormats/treasure_table_file.h"
 #include "egolib/FileFormats/wawalite_file.h"
 
 extern tile_dictionary_t tile_dict;
