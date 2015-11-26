b97191f88bd451c32bb7e580c0597ca2cec37c8b
Johan Jansen <jnsn.johan@gmail.com>
2015-11-26 22:37:48 +0100

    CharacterMatrix: Fixes after merge

--- game/src/game/CharacterMatrix.c
+++ game/src/game/CharacterMatrix.c
@@ -171,8 +171,8 @@ bool chr_get_matrix_cache( Object * pchr, matrix_cache_t& mc_tmp )
             mc_tmp.valid   = true;
             SET_BIT( mc_tmp.type_bits, MAT_CHARACTER );  // add in the MAT_CHARACTER-type data for the object we are "connected to"
 
-            mc_tmp->rotate[kX] = Ego::Math::clipBits<16>( ptarget->ori.map_twist_facing_x - MAP_TURN_OFFSET );
-            mc_tmp->rotate[kY] = Ego::Math::clipBits<16>( ptarget->ori.map_twist_facing_y - MAP_TURN_OFFSET );
+            mc_tmp.rotate[kX] = Ego::Math::clipBits<16>( ptarget->ori.map_twist_facing_x - MAP_TURN_OFFSET );
+            mc_tmp.rotate[kY] = Ego::Math::clipBits<16>( ptarget->ori.map_twist_facing_y - MAP_TURN_OFFSET );
             mc_tmp.rotate[kZ] = ptarget->ori.facing_z;
 
             mc_tmp.pos = ptarget->getPosition();
@@ -394,8 +394,8 @@ bool apply_matrix_cache( Object * pchr, matrix_cache_t& mc_tmp )
 
                 mcache.grip_scale = mcache.self_scale;
 
-                mcache->rotate[kX] = Ego::Math::clipBits<16>( pchr->ori.map_twist_facing_x - MAP_TURN_OFFSET );
-                mcache->rotate[kY] = Ego::Math::clipBits<16>( pchr->ori.map_twist_facing_y - MAP_TURN_OFFSET );
+                mcache.rotate[kX] = Ego::Math::clipBits<16>( pchr->ori.map_twist_facing_x - MAP_TURN_OFFSET );
+                mcache.rotate[kY] = Ego::Math::clipBits<16>( pchr->ori.map_twist_facing_y - MAP_TURN_OFFSET );
                 mcache.rotate[kZ] = pchr->ori.facing_z;
 
                 mcache.pos =pchr->getPosition();
@@ -975,8 +975,8 @@ void make_one_character_matrix( const ObjectRef ichr )
 
         pinst.matrix_cache.self_scale = Vector3f(pchr->fat, pchr->fat, pchr->fat);
 
-        pinst->matrix_cache.rotate[kX] = Ego::Math::clipBits<16>( pchr->ori.map_twist_facing_x - MAP_TURN_OFFSET );
-        pinst->matrix_cache.rotate[kY] = Ego::Math::clipBits<16>( pchr->ori.map_twist_facing_y - MAP_TURN_OFFSET );
+        pinst.matrix_cache.rotate[kX] = Ego::Math::clipBits<16>( pchr->ori.map_twist_facing_x - MAP_TURN_OFFSET );
+        pinst.matrix_cache.rotate[kY] = Ego::Math::clipBits<16>( pchr->ori.map_twist_facing_y - MAP_TURN_OFFSET );
         pinst.matrix_cache.rotate[kZ] = pchr->ori.facing_z;
 
         pinst.matrix_cache.pos = pchr->getPosition();
--- game/src/game/script_functions.c
+++ game/src/game/script_functions.c
@@ -1897,7 +1897,7 @@ Uint8 scr_SpawnCharacter( script_state_t& state, ai_state_t& self )
 
 	Vector3f pos = Vector3f(float(state.x), float(state.y), 0.0f);
 
-    std::shared_ptr<Object> pchild = _currentModule->spawnObject(pos, pchr->getProfileID(), pchr->team, 0, Ego::Math::clipBits<16>( state.turn ), "", INVALID_CHR_REF);
+    std::shared_ptr<Object> pchild = _currentModule->spawnObject(pos, pchr->getProfileID(), pchr->team, 0, Ego::Math::clipBits<16>( state.turn ), "", ObjectRef::Invalid);
     returncode = pchild != nullptr;
 
     if ( !returncode )
@@ -5339,7 +5339,7 @@ Uint8 scr_SpawnCharacterXYZ( script_state_t& state, ai_state_t& self )
 
 	Vector3f pos = Vector3f(float(state.x), float(state.y), float(state.distance));
 
-    std::shared_ptr<Object> pchild = _currentModule->spawnObject( pos, pchr->getProfileID(), pchr->team, 0, Ego::Math::clipBits<16>( state.turn ), "", INVALID_CHR_REF );
+    std::shared_ptr<Object> pchild = _currentModule->spawnObject( pos, pchr->getProfileID(), pchr->team, 0, Ego::Math::clipBits<16>( state.turn ), "", ObjectRef::Invalid );
     if (pchild == nullptr)
     {
 		Log::get().warn("%s:%d: object %s failed to spawn a copy of itself\n", __FILE__, __LINE__, pchr->getName().c_str() );
@@ -5381,7 +5381,7 @@ Uint8 scr_SpawnExactCharacterXYZ( script_state_t& state, ai_state_t& self )
 			float(state.distance)
         );
 
-    const std::shared_ptr<Object> pchild = _currentModule->spawnObject(pos, static_cast<PRO_REF>(state.argument), pchr->team, 0, Ego::Math::clipBits<16>(state.turn), "", INVALID_CHR_REF);
+    const std::shared_ptr<Object> pchild = _currentModule->spawnObject(pos, static_cast<PRO_REF>(state.argument), pchr->team, 0, Ego::Math::clipBits<16>(state.turn), "", ObjectRef::Invalid);
 
     if ( !pchild )
     {
