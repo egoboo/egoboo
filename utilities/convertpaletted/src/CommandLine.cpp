a70cbfd14182a1113a30f405baa0f9747d2cae23
Michael Heilmann <michaelheilmann@primordialmachine.com>
2015-12-21 22:39:07 +0100

    Improve tooling

---
+++ utilities/convertpaletted/src/CommandLine.cpp
@@ -0,0 +1,145 @@
+#include "CommandLine.hpp"
+
+#include <functional>
+#include <memory>
+#include <vector>
+
+namespace CommandLine {
+
+using namespace std;
+
+Option::Option(Type type) : type(type) {}
+
+Option::Type Option::getType() const {
+    return type;
+}
+
+UnnamedValue::UnnamedValue(const string& value)
+    : Option(Type::UnnamedValue), value(value) {}
+
+const string& UnnamedValue::getValue() const {
+    return value;
+}
+
+NamedValue::NamedValue(const string& name, const string& value)
+    : Option(Type::NamedValue), name(name), value(value) {}
+
+const string& NamedValue::getName() const {
+    return name;
+}
+
+const string& NamedValue::getValue() const {
+    return value;
+}
+
+Switch::Switch(const String& name, bool on)
+    : Option(Type::Switch), name(name), on(on) {}
+
+const string& Switch::getName() const {
+    return name;
+}
+
+bool Switch::isOn() const {
+    return on;
+}
+
+Error::Error(const string& message)
+    : Option(Type::Error), message(message) {}
+
+const string& Error::getMessage() const {
+    return message;
+}
+
+struct Parser {
+    typedef typename function<bool(const string::const_iterator&, const string::const_iterator&, const string::const_iterator&)> Predicate;
+    /// @brief Get if the current character fulfills the specified property.
+    /// @return @a true if the current input character fulfills the specified property,
+    ///         @a false otherwise
+    static bool test(string::const_iterator& at, const string::const_iterator& begin,
+              const string::const_iterator& end, Predicate predicate) {
+        return predicate(at, begin, end);
+    }
+    /// @brief Get if the current character fulfills the specified property.
+    /// @return @a true if the current input character fulfills the specified property,
+    ///         @a false otherwise
+    static bool test(string::const_iterator& at, const string::const_iterator& begin,
+              const string::const_iterator& end, char chr) {
+        return test(at, begin, end, [chr](const string::const_iterator& at, const string::const_iterator& begin, const string::const_iterator& end) {
+            if (at == end) {
+                return false;
+            }
+            if (*at != chr) {
+                return false;
+            }
+            return true;
+        });
+    }
+    static bool expect(string::const_iterator& at, const string::const_iterator& begin,
+                       const string::const_iterator& end, char chr) {
+        if (test(at, begin, end, chr)) {
+            at++;
+            return true;
+        }
+        return false;
+    }
+    static shared_ptr<Option> parse(const string& argument) {
+        using namespace std;
+        const auto begin = argument.cbegin(),
+            end = argument.cend();
+        auto p = begin;
+        if (!Parser::test(p, begin, end, '-')) {
+            auto q = p;
+            while (q != end) {
+                q++;
+            }
+            return make_shared<UnnamedValue>(string(p, q));
+        }
+        // `--`
+        if (!Parser::expect(p, begin, end, '-')) {
+            return make_shared<Error>("expected '`-`'");
+        }
+        if (!Parser::expect(p, begin, end, '-')) {
+            return make_shared<Error>("expected '`-`'");
+        }
+        // name
+        auto q = p;
+        while (q != end && *q != '=') {
+            q++;
+        }
+        if (q == end || q == p) {
+            return make_shared<Error>("expected 'name'");
+        }
+        const string name = string(p, q);
+        // `=`?
+        if (!Parser::test(q, begin, end, '=')) {
+            return make_shared<Switch>(name, true);
+        }
+        q++;
+        // <value>
+        p = q;
+        if (p == end) {
+            return make_shared<Error>("expected 'value'");
+        }
+        q = p;
+        while (q != end) {
+            q++;
+        }
+        if (q == p) {
+            return make_shared<Error>("empty value");
+        }
+        const string value = string(p, q);
+        return make_shared<NamedValue>(name, value);
+    }
+};
+
+using namespace std;
+
+vector<shared_ptr<Option>> parse(int argc, char **argv) {
+    vector<shared_ptr<Option>> result;
+    for (int i = 1; i < argc; ++i) {
+        result.push_back(Parser::parse(argv[i]));
+    }
+    return result;
+}
+
+} // namespace CommandLine
--- utilities/convertpaletted/src/convertpaletted.cpp
+++ utilities/convertpaletted/src/convertpaletted.cpp
@@ -1,227 +1,72 @@
-#include <iostream>
-#include <string>
-#include <memory>
+#include "ConvertPaletted.hpp"
 
-#include <deque>
+#include "Filters.hpp"
+
 #include <SDL.h>
 #include <SDL_image.h>
-#if defined(_WIN32)
-#include <Windows.h>
-#define REGEX_DIRSEP "[/\\\\]"
-#else
-#include <dirent.h>
-#include <sys/stat.h>
-#define REGEX_DIRSEP "/"
-#endif
 
-/**********************************************************************************************************/
 
-struct FileSystem
-{
-    static std::string getDirectorySeparator()
-    {
-#if defined(_WIN32)
-        return "\\";
-#else
-        return "/";
-#endif
-    }
-    /**
-     * @brief
-     *  Get the current working directory of this process.
-     * @return
-     *  the current working directory of this process
-     * @throw std::runtime_error
-     *  if the current working directory can not be obtained
-     * @throw std::bad_alloc
-     *  if an out of memory situation occurred
-     */
-    static std::string getWorkingDirectory()
-    {
-#if _WIN32
-        auto length = GetCurrentDirectory(0, NULL);
-        if (!length)
-        {
-            throw std::runtime_error("unable to obtain working directory");
+namespace Tools {
+
+using namespace std;
+using namespace CommandLine;
+
+ConvertPaletted::ConvertPaletted()
+    : Editor::Tool("ConvertPaletted") {}
+
+ConvertPaletted::~ConvertPaletted() {}
+
+void ConvertPaletted::run(const vector<shared_ptr<Option>>& arguments) {
+	if (arguments.size() == 1) {
+		ostringstream os;
+		os << "wrong number of arguments" << endl;
+		throw runtime_error(os.str());
+	}
+
+	SDL_Init(0);
+	IMG_Init(IMG_INIT_PNG);
+	deque<string> queue;
+	RegexFilter filter("^(?:.*" REGEX_DIRSEP ")?(?:tris|tile)[0-9]+\\.bmp$");
+	/// @todo Do *not* assume the path is relative. Ensure that it is absolute by a system function.
+	for (const auto& argument : arguments) {
+        if (argument->getType() != Option::Type::UnnamedValue) {
+            ostringstream os;
+            os << "unrecognized argument" << endl;
+            throw runtime_error(os.str());
         }
-        auto buffer = std::make_unique<char[]>(length + 1);
-        length = GetCurrentDirectory(length + 1, buffer.get());
-        if (!length)
-        {
-            throw std::runtime_error("unable to obtain working directory");
-        }
-        return std::string(buffer.get());
-#else
-        // not really needed
-        return ".";
-#endif
-    }
-    static std::string sanitize(const std::string& pathName)
-    {
-#ifdef _WIN32
-        char buffer[MAX_PATH+1];
-        auto result = GetFullPathName(pathName.c_str(),
-                                      MAX_PATH+1,
-                                      buffer,
-                                      NULL);
-        if (!result)
-        {
-            throw std::runtime_error("unable to sanitize path name");
-        }
-        return std::string(buffer);
-#else
-        // not really needed
-        return pathName;
-#endif
-    }
-};
-
-/**********************************************************************************************************/
-
-enum class PathStat {
-    FILE,
-    DIRECTORY,
-    OTHER,
-    FAILURE
-};
-
-void convert(const std::string &fileName);
-void recurDir(const std::string &dirName, std::deque<std::string> &queue);
-PathStat stat(const std::string &pathName);
-
-/// Filter: accept if string ends with ".bmp".
-struct bitmap_filter : public std::unary_function<std::string, bool> {
-
-    bitmap_filter() {}
-    bool operator()(const std::string &s) const {
-        return s.rfind(".bmp") != std::string::npos;
-    }
-};
-
-#include "filters.hpp"
-#include <sstream>
-
-namespace ConvertPaletted {
-	void run(int argc, char **argv) {
-		if (argc == 1) {
-			std::ostringstream os;
-			os << "wrong number of arguments" << std::endl;
-			throw std::runtime_error(os.str());
-		}
-
-		SDL_Init(0);
-		IMG_Init(IMG_INIT_PNG);
-		std::deque<std::string> queue;
-		RegexFilter filter("^(?:.*" REGEX_DIRSEP ")?(?:tris|tile)[0-9]+\\.bmp$");
-		/// @todo Do *not* assume the path is relative. Ensure that it is absolute by a system function.
-		for (int i = 1; i < argc; i++) {
-			queue.emplace_back(FileSystem::sanitize(argv[i]));
-		}
-		while (!queue.empty()) {
-			std::string path = queue[0];
-			queue.pop_front();
-			switch (stat(path)) {
-			case PathStat::FILE:
-				if (filter(path)) {
-					convert(path);
-				}
-				break;
-			case PathStat::DIRECTORY:
-				recurDir(path, queue);
-				break;
-			case PathStat::FAILURE:
-				break; // stat complains
-			default:
-				{
-					std::ostringstream os;
-					os << "skipping '" << path << "' - not a file or directory" << std::endl;
-					std::cerr << os.str();
-				}
+        auto& pathnameArgument = static_pointer_cast<UnnamedValue>(argument);
+		queue.emplace_back(FileSystem::sanitize(pathnameArgument->getValue()));
+	}
+	while (!queue.empty()) {
+		string path = queue[0];
+		queue.pop_front();
+		switch (FileSystem::stat(path)) {
+		case FileSystem::PathStat::File:
+			if (filter(path)) {
+				convert(path);
+			}
+			break;
+		case FileSystem::PathStat::Directory:
+			FileSystem::recurDir(path, queue);
+			break;
+        case FileSystem::PathStat::Failure:
+			break; // stat complains
+		default:
+			{
+				ostringstream os;
+				os << "skipping '" << path << "' - not a file or directory" << endl;
+				cerr << os.str();
 			}
 		}
-		IMG_Quit();
-		SDL_Quit();
 	}
+	IMG_Quit();
+	SDL_Quit();
 }
-
-#if defined(_WIN32)
-PathStat stat(const std::string &pathName) {
-    DWORD attributes = GetFileAttributes(pathName.c_str());
-    if (0xFFFFFFFF == attributes) {
-        std::cerr << pathName << ": " << "GetFileAttributes failed" << std::endl;
-        return PathStat::FAILURE;
-    }
-    DWORD rejectedMask = (FILE_ATTRIBUTE_TEMPORARY | FILE_ATTRIBUTE_READONLY | FILE_ATTRIBUTE_OFFLINE | FILE_ATTRIBUTE_REPARSE_POINT | FILE_ATTRIBUTE_SYSTEM | FILE_ATTRIBUTE_VIRTUAL);
-    if (attributes & rejectedMask) {
-        return PathStat::OTHER;
-    }
-    else if (attributes & FILE_ATTRIBUTE_DIRECTORY) {
-        return PathStat::DIRECTORY;
-    }
-    // It is not rejected and it is not a directory.
-    if (attributes == FILE_ATTRIBUTE_NORMAL || attributes & FILE_ATTRIBUTE_ARCHIVE) {
-        return PathStat::FILE;
-    } else {
-        return PathStat::OTHER;
-    }
-}
-#else
-PathStat stat(const std::string &pathName) {
-    struct stat out;
-    int success = lstat(pathName.c_str(), &out);
-    if (success == -1) {
-        std::cerr << pathName << ": " << strerror(errno) << std::endl;
-        return PathStat::FAILURE;
-    }
-    if (S_ISREG(out.st_mode)) return PathStat::FILE;
-    if (S_ISDIR(out.st_mode)) return PathStat::DIRECTORY;
-    return PathStat::OTHER;
-}
-#endif
-
-#if defined(_WIN32)
-void recurDir(const std::string &pathName, std::deque<std::string> &queue) {
-    WIN32_FIND_DATA ffd;
-    HANDLE hFind = FindFirstFile((pathName + "\\*").c_str(), &ffd);
-    if (INVALID_HANDLE_VALUE == hFind) {
-        std::cerr << pathName << ": " << "FindFirstFile failed" << std::endl;
-        return;
-    }
-    do {
-        if (ffd.cFileName[0] == '.')
-        {
-            if ((ffd.cFileName[1] == '.' && ffd.cFileName[2] == '\0') || ffd.cFileName[1] == '\0')
-            {
-                continue;
-            }
-        }
-        std::string path = pathName + "\\" + ffd.cFileName;
-        queue.push_back(path);
-    } while (FindNextFile(hFind, &ffd) != 0);
-    FindClose(hFind);
-}
-#else
-void recurDir(const std::string &pathName, std::deque<std::string> &queue) {
-    DIR *dir = opendir(pathName.c_str());
-    if (!dir) {
-        std::cerr << pathName << ": " << strerror(errno) << std::endl;
-        return;
-    }
-    while (dirent *aFile = readdir(dir)) {
-        if (aFile->d_name[0] == '.') continue;
-        std::string path = pathName + "/" + aFile->d_name;
-        queue.push_back(path);
-    }
-    closedir(dir);
-}
-#endif
 
 /// Perform a dry-run, just print the files which would have been converted.
 static bool g_dryrun = false;
 
-
-
-void convert(const std::string &fileName) {
+void ConvertPaletted::convert(const std::string &fileName) {
     if (fileName.rfind(".bmp") == std::string::npos) return;
     std::string out = fileName;
     size_t extPos = out.rfind('.');
@@ -239,8 +84,7 @@ void convert(const std::string &fileName) {
             std::cout << fileName << ": converting [" << uint16_t(r) << ", " << uint16_t(g) << ", " <<
                 uint16_t(b) << ", " << uint16_t(a) << "] to alpha" << std::endl;
             SDL_SetColorKey(surf, SDL_TRUE, 0);
-        }
-        else {
+        } else {
             std::cout << fileName << ": no palette" << std::endl;
         }
         SDL_Surface *other = SDL_ConvertSurfaceFormat(surf, SDL_PIXELFORMAT_RGBA8888, 0);
@@ -259,9 +103,9 @@ void convert(const std::string &fileName) {
         if (IMG_SavePNG(other, out.c_str()) == -1)
             std::cerr << "Couldn't save " << out << ":" << IMG_GetError() << std::endl;
         SDL_FreeSurface(other);
-    }
-    else
-    {
+    } else {
         std::cout << "converting " << fileName << " to " << out << std::endl;
     }
 }
+
+} // namespace Tools
