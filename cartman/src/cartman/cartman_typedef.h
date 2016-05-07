9922e534764ba4b66259eeb34cb8491c8efc9f8b
Michael Heilmann <michaelheilmann@primordialmachine.com>
2016-05-07 03:10:09 +0200

    Cartman: Add Point2i, Size2i, and Vector2i

--- cartman/src/cartman/cartman_typedef.h
+++ cartman/src/cartman/cartman_typedef.h
@@ -26,114 +26,172 @@
 
 #include "cartman/cartman_config.h"
 
-namespace Cartman {
+#define Add(ResultType, OperandType0, OperandType1) \
+    ResultType operator+=(const OperandType1& other) { \
+        (*this) = (*this) + other; \
+        return *this;\
+    }
+#define Subtract(ResultType, OperadnType0, OperandType1) \
+    ResultType operator-=(const OperandType1& other) { \
+        (*this) = (*this) - other; \
+        return *this; \
+    }
+#define NotEqualTo(OperandType0,OperandType1) \
+    bool operator!=(const OperandType1& other) const { \
+        return !(*this == other); \
+    }
 
-struct Size2i {
+namespace Ego {
+namespace Math {
+namespace Discrete {
+
+template <typename T, typename Enabled = void>
+struct Size2;
+
+template <typename T>
+struct Size2<T, std::enable_if_t<std::is_same<T, int>::value>> {
+private:
     int width, height;
+
 public:
-    Size2i(int width, int height) : width(width), height(height) {}
-    Size2i(const Size2i& other) : width(other.width), height(other.height) {}
+    Size2() : width(), height() {}
+    Size2(T width, T height) : width(width), height(height) {}
+    Size2(const Size2<T>& other) : width(other.width), height(other.height) {}
+
 public:
-    const Size2i& operator=(const Size2i& other) {
+    const Size2<T>& operator=(const Size2<T>& other) {
         width = other.width;
         height = other.height;
         return *this;
     }
-public:
-    bool operator==(const Size2i& other) const {
+    bool operator==(const Size2<T>& other) const {
         return width == other.width
             && height == other.height;
     }
-    bool operator!=(const Size2i& other) const {
-        return width != other.width
-            || height != other.height;
-    }
-};
 
-struct Vector2i {
-private:
-    int x, y;
 public:
-    Vector2i() :
-        x(0), y(0) {}
-    Vector2i(int x, int y) :
-        x(x), y(y) {}
-    Vector2i(const Vector2i& other) :
-        x(other.x), y(other.y) {}
-    const int& operator()(size_t i) const {
-        switch (i) {
-            case 0:
-                return x;
-            case 1:
-                return y;
-            default:
-                throw std::invalid_argument("i > 1");
-        }
-    }
-    int& operator()(size_t i) {
-        switch (i) {
-            case 0:
-                return x;
-            case 1:
-                return y;
-            default:
-                throw std::invalid_argument("i > 1");
-        }
-    }
-};
-}
+    // Derived operators
+    NotEqualTo(Size2<T>, Size2<T>);
 
-namespace Cartman {
-struct Point2i {
+public:
+    T getWidth() const {
+        return width;
+    }
+    T getHeight() const {
+        return height;
+    }
+
+}; // struct Size2
+
+template <typename T, typename Enabled = void>
+struct Vector2;
+
+template <typename T>
+struct Vector2<T, std::enable_if_t<std::is_same<T, int>::value>> {
 private:
-    int x, y;
+    T x, y;
+
 public:
-    Point2i() :
-        x(0), y(0) {}
-    Point2i(int x, int y) :
-        x(x), y(y) {}
-    Point2i(const Point2i& other) :
-        x(other.x), y(other.y) {}
+    Vector2() : x(), y() {}
+    Vector2(T x, T y) : x(x), y(y) {}
+    Vector2(const Vector2<T>& other) : x(other.x), y(other.y) {}
+
 public:
-    Point2i& operator=(const Point2i& other) {
+    const Vector2<T>& operator=(const Vector2<T>& other) {
         x = other.x;
         y = other.y;
         return *this;
     }
-public:
-    bool operator==(const Point2i& other) const {
+    Vector2<T> operator+(const Vector2<T>& other) const {
+        return Vector2<T>(x + other.getX(), y + other.getY());
+    }
+    Vector2<T> operator-(const Vector2<T>& other) const {
+        return Vector2<T>(x - other.getX(), y - other.getY());
+    }
+    bool operator==(const Vector2<T>& other) const {
         return x == other.x
             && y == other.y;
     }
-    bool operator!=(const Point2i& other) const {
-        return x != other.x
-            || y != other.y;
-    }
+
 public:
-    void translate(const Vector2i& t) {
-        x += t(0);
-        y += t(1);
-    }
+    // Derived operators
+    Add(Vector2<T>, Vector2<T>, Vector2<T>);
+    Subtract(Vector2<T>, Vector2<T>, Vector2<T>);
+    NotEqualTo(Vector2<T>, Vector2<T>);
+
 public:
-    const int& operator()(size_t i) const {
-        switch (i) {
-            case 0:
-                return x;
-            case 1:
-                return y;
-            default:
-                throw std::invalid_argument("i > 1");
-        }
+    int getX() const {
+        return x;
     }
-    int& operator()(size_t i) {
-        switch (i) {
-            case 0:
-                return x;
-            case 1:
-                return y;
-            default:
-                throw std::invalid_argument("i > 1");
-        }
+    int getY() const {
+        return y;
     }
-};
+
+}; // struct Vector2
+
+template <typename T, typename Enabled = void>
+struct Point2;
+
+template <typename T>
+struct Point2<T, std::enable_if_t<std::is_same<T, int>::value>> {
+private:
+    T x, y;
+
+public:
+    Point2() : x(0), y(0) {}
+    Point2(T x, T y) : x(x), y(y) {}
+    Point2(const Point2<T>& other) : x(other.x), y(other.y) {}
+
+public:
+    const Point2<T>& operator=(const Point2<T>& other) {
+        x = other.x;
+        y = other.y;
+        return *this;
+    }
+
+public:
+    bool operator==(const Point2<T>& other) const {
+        return x == other.x
+            && y == other.y;
+    }
+
+public:
+    Point2<T> operator+(const Vector2<T>& t) const {
+        return Point2i(x + t.getX(), y + t.getY());
+    }
+    Point2<T> operator-(const Vector2<T>& t) const {
+        return Point2<T>(x - t.getX(), y - t.getY());
+    }
+    Vector2<T> operator-(const Point2<T>& other) const {
+        return Point2<T>(x - other.getX(), y - other.getY());
+    }
+
+public:
+    // Derived operators
+    Add(Point2<T>, Point2<T>, Vector2<T>);
+    Subtract(Point2<T>, Point2<T>, Vector2<T>);
+    NotEqualTo(Point2<T>, Point2<T>);
+
+public:
+    int getX() const {
+        return x;
+    }
+    int getY() const {
+        return y;
+    }
+
+}; // struct Point2
+
+} // namespace Discrete
+} // namespace Math
+} // namespace Ego
+
+#undef Subtract
+#undef NotEqualTo
+#undef Add
+
+namespace Cartman {
+typedef Ego::Math::Discrete::Vector2<int> Vector2i;
+typedef Ego::Math::Discrete::Point2<int> Point2i;
+typedef Ego::Math::Discrete::Size2<int> Size2i;
 }
