43b45c8fa30eaf305008ac5aacdff2e7a148c174
Johan Jansen <jnsn.johan@gmail.com>
2016-06-11 14:40:45 +0200

    Make: Various compile fixes for gcc-6

--- game/src/game/Entities/Particle.cpp
+++ game/src/game/Entities/Particle.cpp
@@ -406,7 +406,6 @@ void Particle::updateWater()
                         static constexpr int RIPPLEAND = 15;          ///< How often ripples spawn
                         if (0 == ((update_wld + _particleID.get()) & (RIPPLEAND << 1)))
                         {
-
                             spawn_valid = true;
                             global_pip_index = LocalParticleProfileRef(PIP_RIPPLE);
                         }
