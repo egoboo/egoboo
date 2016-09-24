5db79e919b13467a72d42cb35849752823819ddb
Michael Heilmann <michaelheilmann@primordialmachine.com>
2016-09-24 11:04:42 +0200

    Runtime: Fix buffer

--- egolib/src/egolib/Script/Buffer.cpp
+++ egolib/src/egolib/Script/Buffer.cpp
@@ -45,38 +45,52 @@ size_t Buffer::getCapacity() const {
     return _capacity;
 }
 
-size_t Buffer::getMaxCapacity() const {
+size_t Buffer::getMaximalCapacity() const {
     return std::numeric_limits<size_t>::max();
 }
 
-void Buffer::increaseCapacity(size_t req) {
-    if (0 == req) return;
-    // This buffer is re-used and does not shrink.
-    // Nevertheless we want to avoid frequent reallocations and if the requested additional capacity is too small,
-    // add a bit more.
-    size_t best = req;
-    if (best < 5012) {
-        best = 5012;
-    } else {
-        best = req;
+void Buffer::increaseCapacity(size_t requiredAdditionalCapacity) {
+    if (0 == requiredAdditionalCapacity) {
+        return;
     }
-    // It is unlikely that the requested additional capacity ever exceeds
-    // the maximum available additional capacity. However if it does ...
-    if (getMaxCapacity() - best < _capacity) {
-        // ... compute the available additional capacity.
-        size_t avl = getMaxCapacity() - _capacity;
-        // If the available additional capacity is smaller than then requested additional capacity,
-        // raise an exception.
-        if (avl < req) {
-            throw std::bad_array_new_length();
-        }
-        best = avl;
+    // Compute the maximum additional capacity.
+    size_t maximumAdditionalCapacity = getMaximalCapacity() - getCapacity();
+    // If the required additional capacity is greater than the maximum additional capacity ...
+    if (maximumAdditionalCapacity < requiredAdditionalCapacity) {
+        // ...raise an exception.
+        Id::RuntimeErrorException(__FILE__, __LINE__, "required additional capacity exceeds maximum additional capacity");
     }
-    char *newElements = new char[_capacity + best];
+    // Compute the desired additional capacity.
+    // Note that if getCapacity() * 2 overflows, then we have a wrap around. This does not cause any harm.
+    size_t desiredAdditionalCapacity = getCapacity() > 0 ? getCapacity() * 2 : 8;
+    // The desired additional capacity might be smaller, equal, or greater than the required additional capacity.
+    // Take the maximum of both.
+    size_t additionalCapacity = std::max(desiredAdditionalCapacity, requiredAdditionalCapacity);
+    // The additional capacity may not exceed the maximum additional capacity.
+    additionalCapacity = std::min(maximumAdditionalCapacity, additionalCapacity);
+
+    // Compute the new capacity.
+    size_t newCapacity = getCapacity() + additionalCapacity;
+    
+    char *newElements = new char[newCapacity];
     memcpy(newElements, _elements, _size);
     delete[] _elements;
     _elements = newElements;
-    _capacity += best;
+    _capacity = newCapacity;
+}
+
+void Buffer::ensureFreeCapacity(size_t requiredFreeCapacity) {
+    // (1) Compute the available free capacity.
+    size_t availableFreeCapacity = getCapacity() - getSize();
+    // (2) If the required free capacity is smaller than or equal to the available free capacity ...
+    if (requiredFreeCapacity <= availableFreeCapacity) {
+        // ... do nothing.
+        return;
+    }
+    // (3) Otherwise: Compute the additional required free capacity which is guaranteed to be greater than 0 at this point.
+    size_t additionalCapacity = requiredFreeCapacity - availableFreeCapacity;
+    // (4) Increase the capacity by the additional capacity.
+    increaseCapacity(additionalCapacity);
 }
 
 char Buffer::get(size_t index) const {
@@ -95,38 +109,29 @@ std::string Buffer::toString() const {
 }
 
 void Buffer::prepend(char byte) {
-    if (_size == _capacity) {
-        increaseCapacity(1);
-    }
-    memmove(_elements, _elements + 1, _size);
+    ensureFreeCapacity(1);
+    memmove(_elements + 1, _elements, _size);
     _elements[0] = byte;
     _size++;
 }
 
+void Buffer::prepend(const char *bytes, size_t numberOfBytes) {
+    if (0 == numberOfBytes) return;
+    ensureFreeCapacity(numberOfBytes);
+    memmove(_elements + numberOfBytes, _elements, _size);
+    memcpy(_elements, bytes, numberOfBytes);
+    _size += numberOfBytes;
+}
+
 void Buffer::append(char byte) {
-    if (_size == _capacity) {
-        increaseCapacity(1);
-    }
+    ensureFreeCapacity(1);
     _elements[_size++] = byte;
 }
 
 void Buffer::append(const char *bytes, size_t numberOfBytes) {
-    size_t maximalFreeCapacity = SIZE_MAX - _size;
-    // If the maximal free capacity is smaller than the number of Bytes to append ...
-    if (maximalFreeCapacity < numberOfBytes) {
-        // ... then raise an exception.
-        throw std::bad_array_new_length();
-    }
-    size_t capacity = _capacity;
-    size_t requiredCapacity = _size + numberOfBytes;
-    // If the capacity is smaller than the required capacity ...
-    if (capacity < requiredCapacity) {
-        // ... then increase the capacity by the difference of the required capacity and the capacity.
-        increaseCapacity(requiredCapacity - capacity);
-    }
-    for (size_t i = 0, n = numberOfBytes; i < n; ++i) {
-        _elements[i] = bytes[i];
-    }
+    if (0 == numberOfBytes) return;
+    ensureFreeCapacity(numberOfBytes);
+    memcpy(_elements + _size, bytes, numberOfBytes);
     _size += numberOfBytes;
 }
 
@@ -134,9 +139,7 @@ void Buffer::insert(char byte, size_t index) {
     if (index > _size) {
         throw std::out_of_range("index out of range");
     }
-    if (_size == _capacity) {
-        increaseCapacity(1);
-    }
+    ensureFreeCapacity(1);
     size_t toCopy = _size - index;
     if (toCopy) {
         memmove(_elements + index, _elements + index + 1, toCopy);
@@ -146,6 +149,12 @@ void Buffer::insert(char byte, size_t index) {
 
 bool Buffer::isEmpty() const {
     return 0 == getSize();
+}
+
+void Buffer::swap(Buffer& other) noexcept {
+    std::swap(_elements, other._elements);
+    std::swap(_size, other._size);
+    std::swap(_capacity, other._capacity);
 }
 
 } // namespace Script
