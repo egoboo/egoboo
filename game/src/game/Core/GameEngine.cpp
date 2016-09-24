8716346ee813fcf4ba645d5010093cea04d4ac11
zefz <jnsn.johan@gmail.com>
2016-09-24 18:48:36 +0200

    GUI: Fix various warnings after rebasing

--- game/src/game/Core/GameEngine.cpp
+++ game/src/game/Core/GameEngine.cpp
@@ -129,7 +129,7 @@ void GameEngine::start()
         }
 
         // Check if it is time to draw everything
-        if(getMicros() >= _renderTimeout || true)
+        if(getMicros() >= _renderTimeout)
         {
             // Draw the current frame
             renderOneFrame();
